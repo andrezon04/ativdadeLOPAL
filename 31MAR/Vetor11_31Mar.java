c7f138ca6dc1dcff395bbcf385e9d459c624a624
André Dias <diasa0421@gmail.com>
2025-06-10 12:17:34 -0300

    Update Vetor11_31Mar.java

--- 31MAR/Vetor11_31Mar.java
+++ 31MAR/Vetor11_31Mar.java
@@ -3,8 +3,22 @@ package atividadeLOPAL;
 public class Vetor11_31Mar {
 
 	public static void main(String[] args) {
-	
+		int[] numeros = { 1, 2, 3, 4, 5 };
+		int[] caesar = new int[numeros.length];
 
+		caesar[0] = numeros[numeros.length - 1];
+		for (int i = 1; i < numeros.length; i++) {
+			caesar[i] = numeros[i - 1];
+		}
+
+		System.out.print("Vetor original: ");
+		for (int num : numeros) {
+			System.out.print(num + " ");
+		}
+
+		System.out.print("Vetor rotacionado: ");
+		for (int num : caesar) {
+			System.out.print(num + " ");
+		}
 	}
-
 }
